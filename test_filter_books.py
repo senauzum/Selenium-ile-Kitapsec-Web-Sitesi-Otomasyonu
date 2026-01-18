12a407617ae043c8b6d4c5038fab518421be68d8
senauzum <senauzum77@gmail.com>
2026-01-18 23:14:06 +0300

    Replace sleep with explicit wait

--- test_filter_books.py
+++ test_filter_books.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -9,37 +11,45 @@ chromedriver_autoinstaller.install()
 driver=webdriver.Chrome()
 video_url="https://www.kitapsec.com/"
 driver.get(video_url)
+wait = WebDriverWait(driver, 10)
 driver.maximize_window()
-sleep(3)
-driver.find_element(By.XPATH,'/html/body/div[4]/div/div/table/tbody/tr/td[4]/ul/li/a').click()
-sleep(3)
-driver.find_element(By.XPATH,'//*[@id="menuKategoriDiv4"]/table/tbody/tr/td[1]/table/tbody/tr/td[2]/div/a[6]').click()
-sleep(3)
+
+children_book_menu=wait.until( EC.element_to_be_clickable((By.LINK_TEXT,"ÇOCUK KİTABI")))
+children_book_menu.click()
+world_classics_category=wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"Dünya Klasikleri")))
+world_classics_category.click()
+
 driver.execute_script("window.scrollBy(0,1300)")
-sleep(5)
-driver.find_element(By.XPATH,'//*[@id="yayinEvleriListesi"]/a[2]').click()
-sleep(2)
+
+is_bankasi_kultur=wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"İş Bankası Kültür Yayınları (45)")))
+is_bankasi_kultur.click()
+
 driver.execute_script("window.scrollBy(0,1600)")
-sleep(2)
-driver.find_element(By.XPATH,'/html/body/div[5]/div[1]/div/div/div/div[2]/div/div/table/tbody/tr[2]/td/table[2]/tbody/tr[4]/td/div/table/tbody/tr/td[1]/select').click()
-sleep(2)
+
+min_price=wait.until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[5]/div[1]/div/div/div/div[2]/div/div/table/tbody/tr[2]/td/table[2]/tbody/tr[4]/td/div/table/tbody/tr/td[1]/select')))
+min_price.click()
 min_price = Select(driver.find_element(By.NAME, "FiyatBaslangic"))
 min_price.select_by_value("50")
-sleep(1)
-driver.find_element(By.XPATH,'/html/body/div[5]/div[1]/div/div/div/div[2]/div/div/table/tbody/tr[2]/td/table[2]/tbody/tr[4]/td/div/table/tbody/tr/td[3]/select').click()
-sleep(2)
+
+max_price=wait.until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[5]/div[1]/div/div/div/div[2]/div/div/table/tbody/tr[2]/td/table[2]/tbody/tr[4]/td/div/table/tbody/tr/td[3]/select')))
+max_price.click()
 max_price = Select(driver.find_element(By.NAME, "FiyatBitis"))
 max_price.select_by_value("100")
-sleep(1)
-driver.find_element(By.NAME,"B1").click()
-sleep(1)
+
+price_filter_search_button=wait.until(EC.element_to_be_clickable((By.NAME,"B1")))
+price_filter_search_button.click()
+
 driver.execute_script("window.scrollBy(0,250)")
-driver.find_element(By.XPATH,'//*[@id="374610"]/a[1]/img').click()
-sleep(1)
+
+selected_book=wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"Peter Pan J.M.Barrie İş Bankası Kültür Yayınları")))
+selected_book.click()
+
 driver.execute_script("window.scrollBy(0,250)")
-sleep(1)
-driver.find_element(By.XPATH,'//*[@id="https://www.kitapsec.com/Products/Peter-Pan-JMBarrie-Is-Bankasi-Kultur-Yayinlari-374610.html"]/div[1]/div[2]/div[4]/div/div/a[2]').click()
-sleep(1)
-driver.find_element(By.XPATH,'//*[@id="https://www.kitapsec.com/Products/Peter-Pan-JMBarrie-Is-Bankasi-Kultur-Yayinlari-374610.html"]/div[1]/div[2]/div[4]/a[2]').click()
+
+increase_quantity_button=wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"+")))
+increase_quantity_button.click()
+add_to_cart_button=wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"Sepete Ekle")))
+add_to_cart_button.click()
+
 sleep(2)
 driver.quit()
--- test_search_and_go_to_cart.py
+++ test_search_and_go_to_cart.py
@@ -10,30 +10,27 @@ chromedriver_autoinstaller.install()
 driver=webdriver.Chrome()
 video_url="https://www.kitapsec.com/"
 driver.get(video_url)
-
-wait = WebDriverWait(driver, 15)
+wait = WebDriverWait(driver, 10)
 
 print(f"Bulunduğunuz sayfanın linki:{driver.current_url}")
 print(f"Sayfa Başlığı:{driver.title}")
 driver.maximize_window()
-sleep(3)
- 
-search_box=driver.find_element(By.ID,"yeniAramaInput")
-search_box.send_keys("Kürk Mantolu Madonna")
-sleep(1)
-driver.find_element(By.XPATH,'//*[@id="SearchNewForm1"]/a').click()
-sleep(1)
-driver.find_element(By.XPATH,'//*[@id="42699"]/a[2]/span').click()
-sleep(1)
+
+search_input=wait.until(EC.element_to_be_clickable((By.ID,"yeniAramaInput")))
+search_input.send_keys("Kürk Mantolu Madonna")
+
+search_button=wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"ARA")))
+search_button.click()
+
+selected_book=wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"Kürk Mantolu Madonna Yapı Kredi Yayınları")))
+selected_book.click()
+
 driver.execute_script("window.scrollBy(0,250)")
-sleep(2)
-driver.find_element(By.XPATH,'//*[@id="https://www.kitapsec.com/Products/Kurk-Mantolu-Madonna-Yapi-Kredi-Yayinlari-42699.html"]/div[1]/div[2]/div[4]/a[2]').click()
-sleep(1)
+
+wait.until(EC.element_to_be_clickable((By.LINK_TEXT,"Sepete Ekle"))).click()
 
 wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "div.sweet-overlay")))
-
 wait.until(EC.element_to_be_clickable((By.ID, "sptHdCountText"))).click()
-
 
 sleep(2)
 driver.quit()
